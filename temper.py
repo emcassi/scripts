b33c9491337e060f54bfec101d859bfcf95ef8a6
Alex Wayne <alex.wayne.dev@gmail.com>
2024-01-21 01:40:49 -0700

    Added typehints, help helper, and a few comments

--- temper.py
+++ temper.py
@@ -14,38 +14,39 @@ Units:
     K - Kelvin
 """
 
+from collections.abc import Callable
 import sys
 
-def is_valid_unit(unit):
+def is_valid_unit(unit: str):
     """ Check if the provided unit is valid (C, F, or K). """
     return unit in ["C", "F", "K"]
 
-def convert_c_to_f(c):
+def convert_c_to_f(c: float):
     """ Convert Celsius to Fahrenheit. """
     return (c * 9/5) + 32
 
-def convert_c_to_k(c):
+def convert_c_to_k(c: float):
     """ Convert Celsius to Kelvin. """
     return c + 273.15
 
-def convert_f_to_c(f):
+def convert_f_to_c(f: float):
     """ Convert Fahrenheit to Celsius. """
     return (f - 32) * 5/9
 
-def convert_f_to_k(f):
+def convert_f_to_k(f: float):
     """ Convert Fahrenheit to Kelvin. """
     return convert_c_to_k(convert_f_to_c(f))
 
-def convert_k_to_c(k):
+def convert_k_to_c(k: float):
     """ Convert Kelvin to Celsius. """
     return k - 273.15
 
-def convert_k_to_f(k):
+def convert_k_to_f(k: float):
     """ Convert Kelvin to Fahrenheit. """
     return convert_c_to_f(convert_k_to_c(k))
 
 # Conversion dictionary
-conversion_dict = {
+conversion_dict: dict[tuple[str, str], Callable] = {
     ("C", "F"): convert_c_to_f,
     ("C", "K"): convert_c_to_k,
     ("F", "C"): convert_f_to_c,
@@ -54,7 +55,7 @@ conversion_dict = {
     ("K", "F"): convert_k_to_f,
 }
 
-def convert_temp(degrees, input_unit, output_unit):
+def convert_temp(degrees: float, input_unit: str, output_unit: str):
     """ Convert temperature from one unit to another using the conversion dictionary. """
     try:
         conversion_func = conversion_dict[(input_unit, output_unit)]
@@ -63,15 +64,24 @@ def convert_temp(degrees, input_unit, output_unit):
         print("Invalid conversion units")
         exit(1)
 
-def format_answer(answer):
+def format_answer(answer: float):
     """ Format the final answer to two decimal places or as an integer. """
     if answer == int(answer):
         return f"{int(answer)}"
     else:
         return f"{answer:.2f}"
 
-if len(sys.argv) != 3:
+def show_usage():
+    """ Give a hint on how to use the program """
     print("Usage: <input_temperature><input_unit> <output_unit>")
+
+
+if len(sys.argv) != 3:
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+            show_usage()
+            exit(0)
+    show_usage()
     exit(1)
 
 input = sys.argv[1]
@@ -85,14 +95,17 @@ try:
     degrees = float(input[:-1])
 except ValueError:
     print("Invalid input Temperature")
+    show_usage()
     exit(1)
 
 if not is_valid_unit(input_unit):
     print("Invalid Input Unit")
+    show_usage()
     exit(1)
 
 if not is_valid_unit(output_unit):
     print("Invalid Output Unit")
+    show_usage()
     exit(1)
 
 if input_unit == output_unit:
